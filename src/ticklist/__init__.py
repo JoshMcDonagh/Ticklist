84c97027af46d3afd342d4a88fbc128a6c827d77
JoshMcDonagh <jmmarland99@gmail.com>
2026-03-16 00:03:23 +0000

    Add access to task lists via ticklist\__init__.py

---
+++ src/ticklist/__init__.py
@@ -0,0 +1,23 @@
+from src.ticklist.task_list.closed_task_list import ClosedTaskList
+from src.ticklist.task_list.open_task_list import OpenTaskList
+
+_open_task_list:OpenTaskList | None = None
+_closed_task_list:ClosedTaskList | None = None
+
+
+def get_open_task_list() -> OpenTaskList:
+    global _open_task_list
+
+    if _open_task_list is None:
+        _open_task_list = OpenTaskList()
+
+    return _open_task_list
+
+
+def get_closed_task_list() -> ClosedTaskList:
+    global _closed_task_list
+
+    if _closed_task_list is None:
+        _closed_task_list = ClosedTaskList()
+
+    return _closed_task_list
