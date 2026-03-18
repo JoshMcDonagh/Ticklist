d3e5d48641fc7f57c1d671d7b8109f9c5cebfe78
JoshMcDonagh <joshua.mcdonagh.99@gmail.com>
2026-03-18 15:34:24 +0000

    Modify tick command

--- src/ticklist/cli/command/tick.py
+++ src/ticklist/cli/command/tick.py
@@ -2,9 +2,11 @@ from datetime import datetime
 from typing import Dict, Any, List
 
 from src.ticklist import get_open_task_list, get_closed_task_list
-from src.ticklist.cli.command import Command
+from src.ticklist.cli.command import Command, MandatoryArgument, OptionalArgument
 from src.ticklist.schemas import TASK_NO_TYPE
 from src.ticklist.task import Task
+
+_TASK_NO_NAME = "task_no"
 
 
 class Tick(Command):
@@ -17,18 +19,26 @@ class Tick(Command):
         return "Marks task as complete"
 
     @property
-    def mandatory_args(self) -> List[type]:
-        return [TASK_NO_TYPE]
+    def mandatory_args(self) -> Dict[str, MandatoryArgument]:
+        mandatory_args: List[MandatoryArgument] = [MandatoryArgument(
+            name=_TASK_NO_NAME,
+            type=TASK_NO_TYPE
+        )]
+        return self._make_arg_dict(mandatory_args)
 
     @property
-    def optional_args(self) -> Dict[str, type]:
+    def optional_args(self) -> Dict[str, OptionalArgument]:
         return {}
 
-    def execute(self, actual_mandatory_args: list, actual_optional_args: Dict[str, Any]) -> None:
+    def execute(
+            self,
+            actual_mandatory_args: Dict[str, MandatoryArgument],
+            actual_optional_args: Dict[str, OptionalArgument]
+    ) -> None:
         open_task_list = get_open_task_list()
         closed_task_list = get_closed_task_list()
 
-        task_no = str(actual_mandatory_args[0])
+        task_no = str(actual_mandatory_args[_TASK_NO_NAME])
 
         task = open_task_list.get_task(task_no)
         open_task_list.remove_task(task_no)
